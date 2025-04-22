80247c32bbcb16bb102ec546d93e39afb9a1b211
Mickey-Walker51 <steve@caii.co.uk>
2025-04-22 09:16:34 +0100

    Remove old index.js from rootx.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
-      <h1>Welcome to the Three Friend Problem</h1>
-      <p>This is a basic Next.js app. Let’s build something cool!</p>
-    </div>
-  );
-}
+
